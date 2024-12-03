38f6280fd3f94aa97aad44ff420b20e97d97346f
Stoyan Bogdanov <sbogdanov@baylibre.com>
2024-12-03 18:36:53 +0200

    drivers: gpio: max14906: Implement GPIO diagnostic
    Add functionality to GPIO diagnostic needed to allow using
    advanced diagnostic in combination with interrupt form FAULT pin.
    
    Signed-off-by: Stoyan Bogdanov <sbogdanov@baylibre.com>

--- drivers/gpio/gpio_max14906.h
+++ drivers/gpio/gpio_max14906.h
@@ -316,6 +316,11 @@ struct max14906_data {
 		union max14906_global_err glob_err;
 		union max14906_mask mask;
 	} glob;
+#if defined(CONFIG_GPIO_DIAGNOSTIC)
+	sys_slist_t diag_callbacks;
+	struct gpio_callback diag_cb_data;
+	const struct device *dev;
+#endif
 };
 
 #endif
